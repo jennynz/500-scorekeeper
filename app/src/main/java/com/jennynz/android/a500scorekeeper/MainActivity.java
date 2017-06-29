c3b838de98e836ab26cd4b7707cb9ef7b3a9f5b7
Jenny Sahng <jenny.sahng@xero.cm>
2017-06-29 16:39:20 +1200

    Doesn't crash when input is empty

--- app/src/main/java/com/jennynz/android/a500scorekeeper/MainActivity.java
+++ app/src/main/java/com/jennynz/android/a500scorekeeper/MainActivity.java
@@ -3,7 +3,11 @@ package com.jennynz.android.a500scorekeeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import static android.text.TextUtils.isEmpty;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,43 +21,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void incrementA(View view) {
-
-        // Get input view
-        TextView inputTextView = (TextView) findViewById(R.id.teamA_input);
-
-        // Convert input score to int
-        String inputString = inputTextView.getText().toString();
-        int inputValue = Integer.parseInt(inputString);
-
-        // Update score
-        teamA = teamA + inputValue;
-
-        // Get score view and update score
-        TextView scoreTextView = (TextView) findViewById(R.id.teamA_score);
-        scoreTextView.setText(Integer.toString(teamA));
-
-        // Empty input field
-        inputTextView.setText("");
+        updateScore("A");
     }
 
     public void incrementB(View view) {
-
-        // Get input view
-        TextView inputTextView = (TextView) findViewById(R.id.teamB_input);
-
-        // Convert input score to int
-        String inputString = inputTextView.getText().toString();
-        int inputValue = Integer.parseInt(inputString);
-
-        // Update score
-        teamB = teamB + inputValue;
-
-        // Get score view and update score
-        TextView scoreTextView = (TextView) findViewById(R.id.teamB_score);
-        scoreTextView.setText(Integer.toString(teamB));
-
-        // Empty input field
-        inputTextView.setText("");
+        updateScore("B");
     }
 
     public void reset(View view) {
@@ -63,6 +35,51 @@ public class MainActivity extends AppCompatActivity {
         scoreTextViewA.setText(Integer.toString(teamA));
         TextView scoreTextViewB = (TextView) findViewById(R.id.teamB_score);
         scoreTextViewB.setText(Integer.toString(teamB));
+    }
+
+    private void updateScore(String team) {
+        int inputId;
+        int scoreId;
+
+        if (team.equals("A")) {
+            inputId = R.id.teamA_input;
+            scoreId = R.id.teamA_score;
+        } else {
+            inputId = R.id.teamB_input;
+            scoreId = R.id.teamB_score;
+        }
+
+        // Get input view
+        TextView inputTextView = (TextView) findViewById(inputId);
+
+        // Convert input text to string
+        String inputString = inputTextView.getText().toString();
+
+        // Validate that input is not empty
+        if (isEmpty(inputString)) {
+            return;
+        }
+
+        // Convert input string to integer
+        int inputValue = Integer.parseInt(inputString);
+
+        // Update score
+        if (team.equals("A")) {
+            teamA = teamA + inputValue;
+        } else {
+            teamB = teamB + inputValue;
+        }
+
+        // Get score view and update score
+        TextView scoreTextView = (TextView) findViewById(scoreId);
+        if (team.equals("A")) {
+            scoreTextView.setText(Integer.toString(teamA));
+        } else {
+            scoreTextView.setText(Integer.toString(teamB));
+        }
+
+        // Empty input field
+        inputTextView.setText("");
     }
 
 }
