94415b80858b62eee81a588ebf5edc1ce20dd7a4
Jenny Sahng <jenny.sahng@xero.com>
2017-07-10 16:51:25 +1200

    Add back arrow to action bar

--- app/src/main/java/com/jennynz/android/a500scorekeeper/AboutActivity.java
+++ app/src/main/java/com/jennynz/android/a500scorekeeper/AboutActivity.java
@@ -1,9 +1,10 @@
 package com.jennynz.android.a500scorekeeper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
@@ -12,11 +13,25 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView udacityLinkTextView =(TextView)findViewById(R.id.udacity_link);
         udacityLinkTextView.setClickable(true);
         udacityLinkTextView.setMovementMethod(LinkMovementMethod.getInstance());
         String text = "<a href='https://www.udacity.com/course/android-basics-user-input--ud836'>Udacity Android Basics: User Input</a>";
         udacityLinkTextView.setText(Html.fromHtml(text));
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
